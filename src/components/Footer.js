0dd8393cb025b8ec5e1448c4ad529ebbc5d4befc
liezsm <liez.dev@gmail.com>
2022-04-18 19:45:30 +0800

    Added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <h1 className='footer_title'>UNDERCONSTRUCTION</h1>
+    </footer>
+  );
+};
+
+export default Footer;
